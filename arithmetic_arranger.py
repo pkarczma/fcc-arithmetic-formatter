9620e660bf3e45edc6b3581813b8401a3121bbf5
pkarczma <przemyslaw.karczmarczyk@cern.ch>
2020-10-28 10:58:11 +0000

    Exceptions handling

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,17 @@
 def arithmetic_arranger(problems):
+    print(problems)
+    if len(problems) > 4:
+        return "Error: Too many problems."
+    
+    for problem in problems:
+        num1, operator, num2 = problem.split()
+        #print(num1, operator, num2)
+        if operator != "+" and operator != "-":
+            return "Error: Operator must be '+' or '-'."
+        if num1.isdecimal() == False or num2.isdecimal() == False:
+            return "Error: Numbers must only contain digits."
+        if len(num1) > 4 or len(num2) > 4:
+            return "Error: Numbers cannot be more than four digits."
+        
 
-
-    return arranged_problems
+    return
